93f973b6cd955d5a6ce2a7e233260be031749457
Gagan Tayal <gagantayal2003@gmail.com>
2023-01-18 06:44:12 +0530

    weather-APP

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 const express = require('express');
-const https = require('https');
-const app = express();
+const path = require('path');
+const hbs = require('hbs');
 const bodyparser = require('body-parser');
+const app = express();
+const publicdirectory = path.join(__dirname,'public');
+const viewPath = path.join(__dirname,'templates/views');
+const partialPath = path.join(__dirname,'templates/partials');
+app.set("view engine","hbs");
+app.set('views',viewPath)
+hbs.registerPartials(partialPath)
 app.use(bodyparser.urlencoded({extended:true})); 
+app.use(express.static(publicdirectory));
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/index.html");  
+    res.render("index", {
+        title:'weather',
+        name:'gagan'
+    });  
 })
-app.post('/',(req,res)=>{
-    console.log("request received")
-    const query = req.body.cityName
-    const apikey = '13ca0c89d8cd04873921fbc32a542965'
-    const url ='https://api.openweathermap.org/data/2.5/weather?q='+ query + 
-    '&appid='+apikey+'&units=metric'
-    https.get(url,(resp)=>{
-        resp.on('data',(data)=>{
-           const weatherData = JSON.parse(data);
-           const temp = weatherData.main.temp;
-           const description = weatherData.weather[0].description
-            console.log(temp);
-            console.log(description);
-            res.write("<h1>the temperature in  "+query+" is  " + temp + "  degree celcius</h1>");
-            res.write("<h2>the weather description is   "+ description +"  </h2");
-        })
-    })
+app.listen(3000,()=>{console.log("our server is runnig at port 3000");
 })
-app.listen(3000,()=>console.log("our server is runnig at port 3000"))
